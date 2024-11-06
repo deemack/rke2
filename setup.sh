9655cb432d05cbc60f347fc3b55396792e10f7a9
David Mackintosh <dmac0010@monash.student.edu>
2024-11-06 15:39:18 +1000

    Cluster

--- setup.sh
+++ setup.sh
@@ -10,12 +10,14 @@ printf "${CYAN}Please provides some system variables${NC}\n"
 read -p "Enter the hostname: " HOSTNAME
 read -p "Enter the host IP address: " HOSTIP
 
+printf "${GREEN}Updating hosts file${NC}\n
+echo "$HOSTIP $HOSTNAME" | sudo tee -a /etc/hosts
+
 printf "${GREEN}Add entry to fstab for SSD${NC}\n"
 uuid=$(lsblk -no uuid /dev/sda | xargs)
 fstab_entry="UUID=""$uuid"" /mnt/storage ntfs permissions,locale=en_US.utf8 0 2"
 echo $fstab_entry | sudo tee -a /etc/fstab
 sudo mount -a
 
-printf "${GREEN}Updating and Upgrading Linux${NC}\n"
+printf "${GREEN}Updating packages${NC}\n"
 sudo zypper refresh && sudo zypper update -y
-
