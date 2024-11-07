d91d0d3f8ccb76bc58ae59ffdb95c91fce1c5e03
deemack <72137221+deemack@users.noreply.github.com>
2024-11-07 14:32:11 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,20 +5,23 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 os_name=$(grep 'PRETTY_NAME' /etc/os-release | awk -F '=' '{gsub(/"/, "", $2); print $2}')
 
-
 printf "${CYAN}Please provides some system variables${NC}\n"
 read -p "Enter the hostname: " HOSTNAME
 read -p "Enter the host IP address: " HOSTIP
 
+printf "${GREEN}Updating packages${NC}\n"
+sudo zypper refresh && sudo zypper update -y
+
 printf "${GREEN}Updating hosts file${NC}\n"
 echo "$HOSTIP $HOSTNAME" | sudo tee -a /etc/hosts
 
-printf "${GREEN}Add entry to fstab for SSD${NC}\n"
-sudo mkdir -p /mnt/storage
-uuid=$(lsblk -no uuid /dev/sda | xargs)
-fstab_entry="UUID=""$uuid"" /mnt/storage ntfs permissions,locale=en_US.utf8 0 2"
-echo $fstab_entry | sudo tee -a /etc/fstab
-sudo mount -a
-
 printf "${GREEN}Updating packages${NC}\n"
 sudo zypper refresh && sudo zypper update -y
+
+printf "${GREEN}Cloning RKE2 repositorys${NC}\n"
+git clone https://github.com/deemack/homelab.git
+
+printf "${GREEN}System is ready to be provisioned via Ansible${NC}\n"
+cd rke2
+
+printf "${GREEN}Eg. ansible-playbook -i inventory playbooks/site.yaml -K --limit kubedev${NC}\n"
