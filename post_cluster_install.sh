7cbbf77e5dadf6c99a762df3b9005e6f83806912
dave mack <dmackintosh@gmail.com>
2025-09-26 20:32:51 +1000

    Update ansible.cfg

--- post_cluster_install.sh
+++ post_cluster_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo vim ~/.bashrc
-export PATH=/var/lib/rancher/rke2/bin:$PATH
+
+echo 'export PATH=/var/lib/rancher/rke2/bin:$PATH' | sudo tee -a ~/.bashrc
 sudo cp /etc/rancher/rke2/rke2.yaml ~/rke2.yaml
 sudo chown k8sadmin:wheel ~/rke2.yaml
