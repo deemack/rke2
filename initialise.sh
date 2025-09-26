b3b914b4e36841413fc9063311179a8e47b3eec0
dave mack <dmackintosh@gmail.com>
2025-09-26 19:14:23 +1000

    Update ansible.cfg

--- initialise.sh
+++ initialise.sh
@@ -3,4 +3,4 @@
 read -p "Enter the hostname for this RKE2 server: " hostname
 sudo hostnamectl set-hostname $hostname
 
-ansible-playbook deploy_rke2.yml --limit $hostname-K --ask-vault-pass
+ansible-playbook deploy_rke2.yml --limit $hostname -K --ask-vault-pass
